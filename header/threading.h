a273d6a54e358588973ee6c3f546f97a8ec54b40
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-04 13:17:53 +0700

    add more parameters

--- header/threading.h
+++ header/threading.h
@@ -6,12 +6,13 @@
 struct threading_ctx {
     std::thread handler;
     int state;
+    int need_join;
     int th_index;
 };
 
 void init_thread(struct threading_ctx *th);
 int get_free_thread(struct threading_ctx *th);
-void fill_thread(struct threading_ctx *th, int thnum, void (*f)());
+void fill_thread(struct threading_ctx *th, int thnum, void (*f)(int), int acceptfd);
 int clean_thread(struct threading_ctx *th);
 
 #endif
--- threading.cc
+++ threading.cc
@@ -18,8 +18,8 @@ int get_free_thread(struct threading_ctx *th) {
     return -1;
 }
 
-void fill_thread(struct threading_ctx *th, int thnum, void (*f)()) {
-    th[thnum].handler = std::thread(f);
+void fill_thread(struct threading_ctx *th, int thnum, void (*f)(int), int acceptfd) {
+    th[thnum].handler = std::thread(f, acceptfd);
     th[thnum].state = 1;
     th[thnum].th_index = thnum;
 }
