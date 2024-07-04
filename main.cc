7772f6f19cb08872c542f050feadc84f8aa77b5c
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-04 12:25:20 +0700

    blueprint thread done without sigabrt

--- main.cc
+++ main.cc
@@ -1,3 +1,4 @@
+#include "header/server.h"
 #include "header/threading.h"
 #include <stdio.h>
 #include <signal.h>
@@ -7,12 +8,48 @@ volatile sig_atomic_t exit_now = 0;
 struct threading_ctx th[MAX_CONN];
 
 static void signal_cb(int signo) {
-    exit_now = signo;
+    exit_now = 1;
 }
 
+void exit_gracefully(int *sockfd) {
+    sock_cleanup(sockfd);
+    clean_thread(th);
+}
+
+void handle_conn() {
+    printf("handled\n");
+}
 
 void _main() {
-    printf("%d\n", get_free_thread(th));
+    int freethread = 0;
+    int sockfd = sock_init(CONF_PORT);
+    if (sockfd == -1) {
+        fprintf(stderr, "socket error\n");
+        sock_cleanup(&sockfd);
+    }
+
+    while(1) {
+        if (exit_now) {
+            fprintf(stdout, "exitting");
+            exit_gracefully(&sockfd);
+            break;
+        }
+
+        freethread = get_free_thread(th);
+        if (freethread == -1) {
+            fprintf(stderr, "no space available, try increase config.c max connection");
+            exit_gracefully(&sockfd);
+            break;
+        } else {
+            // if ((th[freethread].handler))
+            fill_thread(th, freethread, handle_conn);
+        }
+
+        printf("%d\n", freethread);
+
+        
+        
+    }
 }
 
 int main() {
