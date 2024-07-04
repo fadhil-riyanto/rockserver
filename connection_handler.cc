619fe6e1e9266f2a51fab42484d3d704400d6616
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-04 14:54:44 +0700

    add memory allocation

--- connection_handler.cc
+++ connection_handler.cc
@@ -1,6 +1,35 @@
+#include <cstddef>
+#include <cstdlib>
+#include <cstring>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include "header/connection_handler.h"
 
 void handle_conn(int clientfd) {
-    printf("handled fd %d\n", clientfd);
+
+    size_t length = sizeof(char) * 65535;
+    ssize_t ret;
+
+    char *data = (char*)malloc(length);
+    if (data == NULL) {
+        perror("malloc failed");
+    } else {
+        memset(data, 0, length);
+
+        printf("handled fd %d\n", clientfd);
+        while (1) {
+            ret = recv(clientfd, data, length, 0);
+            data[ret] = '\0';
+
+            printf("client data: %s", data);
+            if (strcmp(data, "exit\n") == 0) {
+                break;
+            }
+        }
+
+    }
+    
+    free(data);
+    close(clientfd);
 }
