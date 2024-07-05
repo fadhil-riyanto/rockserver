25985b1a08be4be6d8ff4932dd4844a60d49e2ef
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-05 18:41:43 +0700

    rename extension

---
+++ epoll_watcher.cc
@@ -0,0 +1,23 @@
+#include "header/epoll_watcher.h"
+#include <sys/epoll.h>
+#include <stdio.h>
+
+int epoll_init(int *epfd) {
+    *epfd = epoll_create1(0);
+    if (*epfd == -1) {
+        fprintf(stderr, "epoll init failed");
+        return -1;
+    } 
+    return 0;
+}
+
+int epoll_watch(int fd, int epfd, struct epoll_event *event) {
+    event->events = EPOLLIN;
+    event->data.fd = fd;
+
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, event) == -1) {
+        fprintf(stderr, "error add epoll fd");
+        return -1;
+    }
+    return 0;
+}
