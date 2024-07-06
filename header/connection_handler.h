99e28606f059dd1f85be0531528de80a8a809530
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-06 21:57:34 +0700

    handleconn add signal header

--- header/connection_handler.h
+++ header/connection_handler.h
@@ -1,6 +1,8 @@
 #ifndef CONNECTION_HANDLER_H
 #define CONNECTION_HANDLER_H
 
-void handle_conn(int clientfd);
+#include <signal.h>
+
+void handle_conn(int clientfd, volatile sig_atomic_t *signal_exit_now);
 
 #endif
