781c9750db5c04382f53f370d0b14acb91b95dd5
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-08 13:33:49 +0700

    test with 2 connection

--- src/config.c
+++ src/config.c
@@ -1,6 +1,6 @@
 #define CONF_PORT 8998
-#define MAX_CONN 10
-#define MAX_EVENTS 10
-#define CHILD_MAXEVENTS 10
+#define MAX_CONN 2
+#define MAX_EVENTS 2
+#define CHILD_MAXEVENTS 2
 
 // future, create inifile
