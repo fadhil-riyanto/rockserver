89b78a49fa092c2ccb438b52161f212e97787dd5
fadhil riyanto <me@fadev.org>
2024-07-17 22:14:33 +0700

    add get command supports
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- test/parse.c
+++ test/parse.c
@@ -4,20 +4,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MAX_PAGESIZE (getpagesize() / 2)
-#define ARR_LEN(x) (sizeof(x) / sizeof(char))
+#define USE_DEBUG_FN 1
+
+#ifdef USE_DEBUG_FN
 #define sdd(x) printf("debug: \"%s\"\n", x);
 #define cdd(x) printf("debug: \"%c\"\n", x);
+#define idd(x) printf("debug: \"%d\"\n", x);
+#endif
+
+#define MAX_PAGESIZE (getpagesize() / 2)
+#define ARR_LEN(x) (sizeof(x) / sizeof(char))
+
 #define OPCODE_LENGTH 3
 #define __hot  __attribute__((hot))
 
 enum rockserver_op {
-        RCK_COMMAND_SET
+        RCK_COMMAND_SET,
+        RCK_COMMAND_GET
 };
 
 struct parse_res 
 {
-        rockserver_op op_code;
+        enum rockserver_op op_code;
         char *op1;
         char *op2;
         char *tmp_opcode;
@@ -54,8 +62,10 @@ static inline __hot void __find_opcode(char* text, struct parse_res *res)
 
 static int __define_opcode(char *op_code_str, struct parse_res *res)
 {
-        if (strncmp(op_code_str, "set", OPCODE_LENGTH)) {
+        if (strncmp(op_code_str, "set", OPCODE_LENGTH) == 0) {
                 res->op_code = RCK_COMMAND_SET;
+        } else if (strncmp(op_code_str, "get", OPCODE_LENGTH) == 0) {
+                res->op_code = RCK_COMMAND_GET;
         } else {
                 return -1;
         }
@@ -74,7 +84,8 @@ static int parse(char* text, int len_alloc, struct parse_res *res)
                 fprintf(stderr, "opcode invalid");
         }
 
-        sdd(res->tmp_opcode);
+        // sdd(res->tmp_opcode);
+        idd(res->op_code);
 
         
         
@@ -92,7 +103,7 @@ static int parse(char* text, int len_alloc, struct parse_res *res)
 
 int main()
 {
-        char *setcommand = "set \"aaaa\" \"bbbbb\"";
+        char *setcommand = "get \"aaaa\" \"bbbbb\"";
 
         struct parse_res res;
         alloc_parse(&res);
