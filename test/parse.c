dc499d1502da673bcf29aacdf8970768dd085798
fadhil riyanto <me@fadev.org>
2024-07-18 10:19:46 +0700

    done test parse with very big string
    test op1: pagesize * 12 = 49152 char
    test op2: pagesize * 12 = 49152 + 3 char
    
    test ended without any memory leak
    
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- test/parse.c
+++ test/parse.c
@@ -7,12 +7,12 @@
 #define USE_DEBUG_FN 1
 
 #ifdef USE_DEBUG_FN
-#define sdd(x) printf("debug: \"%s\"\n", x);
-#define cdd(x) printf("debug: \"%c\"\n", x);
-#define idd(x) printf("debug: \"%d\"\n", x);
+#define sdd(x) printf("debug string: \"%s\"\n", x);
+#define cdd(x) printf("debug char: \"%c\"\n", x);
+#define idd(x) printf("debug int: \"%d\"\n", x);
 #endif
 
-#define MAX_PAGESIZE (getpagesize() / 2)
+#define MAX_PAGESIZE (getpagesize())
 #define ARR_LEN(x) (sizeof(x) / sizeof(char))
 
 #define OPCODE_LENGTH 3
@@ -29,14 +29,27 @@ struct parse_res
         char *op1;
         char *op2;
         char *tmp_opcode;
+        int cur_multiplication1;
+        int cur_multiplication2;
 } __attribute__((__aligned__(32))) ; /* reserved first 32 bit align */
+
+void __pdebug_get_cur_pgsize()
+{
+        idd(MAX_PAGESIZE);
+}
 
 void alloc_parse(struct parse_res *res)
 {
         // alloc first
-        res->op1 = (char*)malloc(MAX_PAGESIZE);
-        res->op2 = (char*)malloc(MAX_PAGESIZE);
-        res->tmp_opcode = (char*)malloc(sizeof(char) * (OPCODE_LENGTH + 1));
+        res->cur_multiplication1 = 1;
+        res->cur_multiplication2 = 1;
+
+        res->op1 = (char*)malloc(MAX_PAGESIZE * res->cur_multiplication1);
+        res->op2 = (char*)malloc(MAX_PAGESIZE * res->cur_multiplication2);
+        res->tmp_opcode = (char*)malloc( sizeof(char) * (OPCODE_LENGTH + 1));
+
+        memset(res->op1, NULL, sizeof(MAX_PAGESIZE * res->cur_multiplication1));
+        memset(res->op2, NULL, sizeof(MAX_PAGESIZE * res->cur_multiplication2));
         
 }
 
@@ -89,7 +102,8 @@ static int __define_opcode(char *op_code_str, struct parse_res *res)
 int parse(char* text, int len_alloc, struct parse_res *res)
 {
         int ret;
-        int op1_req_len, op2_req_len;
+        int op1_req_len = 0;
+        int op2_req_len = 0;
         int turn = 0;
         int quote = 0;
         int c1 = 0;
@@ -105,6 +119,7 @@ int parse(char* text, int len_alloc, struct parse_res *res)
         /* start from offset 3 */
         for(int i = 0; i < len_alloc; i++) {
                 char tmp = *(text + i);
+                
                 if (tmp == '"') {
                         /* pass */
                         quote++;
@@ -115,16 +130,32 @@ int parse(char* text, int len_alloc, struct parse_res *res)
                         if (quote == 1 && tmp != ' ' && turn == 0) {
                                 *(res->op1 + c1) = tmp;
                                 c1++;
+                                op1_req_len++;
                         }
                         if (quote == 3 && tmp != ' ' && turn == 1) {
                                 *(res->op2 + c2) = tmp;
                                 c2++;
+                                op2_req_len++;
+                        }
+                        // idd(res->cur_multiplication1);
+                        /* expand memory */
+                        if (op1_req_len > ((MAX_PAGESIZE * res->cur_multiplication1) - 1)) {
+                                res->cur_multiplication1 = res->cur_multiplication1 + 1;
+                                
+                                res->op1 = (char*)realloc(res->op1, MAX_PAGESIZE * res->cur_multiplication1);
                         }
 
-                        if (quote == 2 && tmp == ' ') {
+                        if (op2_req_len > ((MAX_PAGESIZE * res->cur_multiplication2) - 1)) {
+                                res->cur_multiplication2++;
+                                res->op2 = (char*)realloc(res->op2, MAX_PAGESIZE * res->cur_multiplication2);
+                        }
+
+                        // idd(op2_req_len);
+
+                        if (quote == 2) {
                                 *(res->op1 + c1) = '\0';
                         }
-                        if (quote == 4 && tmp == ' ') {
+                        if (quote == 4) {
                                 *(res->op2 + c2) = '\0';
                         }
                         // sdd("space");
@@ -134,7 +165,7 @@ int parse(char* text, int len_alloc, struct parse_res *res)
 
         // sdd(res->op1);
         // sdd(res->op2);
-
+        
 
         return 0;
 }
