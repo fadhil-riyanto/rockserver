d2e400286b1b7c4dd9e5798fb863848eeec92dfa
fadhil riyanto <me@fadev.org>
2024-07-18 19:39:31 +0700

    small test move char
    Signed-off-by: fadhil riyanto <me@fadev.org>

---
+++ test/left_string.cc
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define USE_DEBUG_FN 1
+#ifdef USE_DEBUG_FN
+#define sdd(x) printf("debug string: \"%s\"\n", x);
+#define cdd(x) printf("debug char: \"%c\"\n", x);
+#define idd(x) printf("debug int: \"%d\"\n", x);
+#endif
+
+static void left_string(char* targetstr, int origin_length)
+{
+        char temp = 0;
+        int c = 0;
+        do {
+                temp = *(targetstr + origin_length + c);
+                /* rewrite memory */
+
+                *(targetstr + origin_length + c) = '\0';
+
+                /* move value to the first, and next */
+                *(targetstr + c) = temp;
+                c++;
+        } while (temp != '\0');
+
+        // sdd(targetstr);
+}
+
+static void __debug_str(char* target)
+{
+        /* test len is 10 */
+
+        for(int i = 0; i < 10; i++) {
+                printf("%c", *(target + i) == '\0' ? '0' : *(target + i));
+        }
+        printf("\n");
+}
+
+int main()
+{
+        char str[10] = {
+                '\0', '\0', 'h', 'e', 'y',
+                '\0', '\0', '\0', '\0', '\0'
+        };
+
+        // printf("%s\n", str);
+        __debug_str(str);
+
+        left_string(str, 2);
+
+        __debug_str(str);
+
+
+}
