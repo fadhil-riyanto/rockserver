b2b8f05b60a104453acc99c69496b7b0731a252d
fadhil riyanto <me@fadev.org>
2024-07-20 23:52:42 +0700

    chage debug function as macro
    this changing impact some unprintable character, which escaped
    
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- src/utils/debugutils.h
+++ src/utils/debugutils.h
@@ -5,4 +5,22 @@
 #define cdd(x) printf("debug char: \"%c\"\n", x);
 #define idd(x) printf("debug int: \"%d\"\n", x);
 
+#define __debug_str(target, approx)                                                     \
+{                                                                               \
+        /* test len is 10 */                                                    \
+                                                                                \
+        for(int i = 0; i < approx; i++) {                                           \
+                if (*(target + i) == '\0') {                                    \
+                        printf("%c", '0');                                      \
+                } else if (*(target + i) == '\r') {                             \
+                        printf("%c%c", '\\', 'r');                              \
+                } else if (*(target + i) == '\n') {                             \
+                        printf("%c%c", '\\', 'n');                              \
+                } else {                                                        \
+                        printf("%c", *(target + i));                            \
+                }                                                               \
+        }                                                                       \
+        printf("\n");                                                           \
+}                                                                               \
+
 #endif
