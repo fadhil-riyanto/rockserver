3ba83fac979e45ad2c545df16e0e401260baae02
fadhil riyanto <me@fadev.org>
2024-07-20 13:38:34 +0700

    test add delimiter on other offset
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- test/senddata.php
+++ test/senddata.php
@@ -8,9 +8,11 @@ if ($fd === false) {
 $result = socket_connect($fd, '127.0.0.1', 8998);
 
 /* test send endfile */
-$sendbuf = "abcpiabcpiabcpiabcpi";
-for($i = 0; $i < 100; $i++) {
-        socket_send($fd, $sendbuf, strlen($sendbuf), 0);
-}
+$sendbuf = "abcpiabcpia\r\n\r\nbcpiabcpi";
+// for($i = 0; $i < 100; $i++) {
+//         socket_send($fd, $sendbuf, strlen($sendbuf), 0);
+// }
+
+socket_send($fd, $sendbuf, strlen($sendbuf), 0);
 
 socket_close($fd);
