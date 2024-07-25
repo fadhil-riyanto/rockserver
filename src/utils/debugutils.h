5db4511aa98f071fbd7cb83038a8c3de5475ed22
fadhil riyanto <me@fadev.org>
2024-07-25 11:36:04 +0700

    show debug line in debug macro
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- src/utils/debugutils.h
+++ src/utils/debugutils.h
@@ -4,15 +4,52 @@
 
 #include <stdio.h>
 
+#ifdef USE_DEBUG_PRINT_LN
+#define sddln(x) printf("debug %d string: \"%s\"\n", __LINE__, x);
+#define cddln(x) printf("debug %d char: \"%c\"\n", __LINE__, x);
+#define iddln(x) printf("debug %d int: \"%d\"\n", __LINE__, x);
+#else
+#define sddln(x)
+#define cddln(x)
+#define iddln(x)
+#endif
+
+#ifdef USE_DEBUG_PRINT
 #define sdd(x) printf("debug string: \"%s\"\n", x);
 #define cdd(x) printf("debug char: \"%c\"\n", x);
 #define idd(x) printf("debug int: \"%d\"\n", x);
+#else
+#define sdd(x)
+#define cdd(x)
+#define idd(x)
+#endif
 
-#define __debug_str(target, approx)                                                     \
+#define __debug_str(target, approx)                                             \
 {                                                                               \
         /* test len is 10 */                                                    \
                                                                                 \
-        for(int i = 0; i < approx; i++) {                                           \
+        printf("[DEBUGSTR]");                                                   \
+        for(int i = 0; i < approx; i++) {                                       \
+                if (*(target + i) == '\0') {                                    \
+                        printf("%c", '0');                                      \
+                } else if (*(target + i) == '\r') {                             \
+                        printf("%c%c", '\\', 'r');                              \
+                } else if (*(target + i) == '\n') {                             \
+                        printf("%c%c", '\\', 'n');                              \
+                } else {                                                        \
+                        printf("%c", *(target + i));                            \
+                }                                                               \
+        }                                                                       \
+        printf("\n");                                                           \
+}            
+
+
+#define __debug_str_ln(target, approx)                                             \
+{                                                                               \
+        /* test len is 10 */                                                    \
+                                                                                \
+        printf("[DEBUGSTR_%d]", __LINE__);                                                   \
+        for(int i = 0; i < approx; i++) {                                       \
                 if (*(target + i) == '\0') {                                    \
                         printf("%c", '0');                                      \
                 } else if (*(target + i) == '\r') {                             \
