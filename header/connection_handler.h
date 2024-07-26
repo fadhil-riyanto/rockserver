e79c913f0c79d5cb52aba3e7d5229d5141b0f8f3
fadhil riyanto <me@fadev.org>
2024-07-26 20:43:17 +0700

    add new struct conn session
    in order save the current session of tcp, we record it
    as sequence based on current instruction
    
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- header/connection_handler.h
+++ header/connection_handler.h
@@ -3,6 +3,23 @@
 
 #include "server.h"
 
+enum rockserver_opcode {
+        OPCODE_SET,
+        OPCODE_GET,
+        OPCODE_DEL
+        
+};
+
+struct state_data {
+        rockserver_opcode opcode;
+        char *op1;
+        char *op2;
+        int complete;
+};
+
+void state_data_init(struct state_data *state_data, server_state_t *server_state);
+void state_data_destroy(struct state_data *state_data);
+
 void handle_conn(int clientfd, server_state_t *server_state, int thread_num);
 
 #endif
